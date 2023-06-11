01528112564a797e3a275e816a3947fc052acc37
takashiono2 <takashiono710@gmail.com>
2023-06-11 18:36:43 +0900

    section3_10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from "./utils/pokemon.js"
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon/";
+
+  useEffect(()=>{
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  },[]);
+
   return (
     <div className="App">
     </div>
